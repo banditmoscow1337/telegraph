12009a42af1f56a21f713a05eaf4a39ef8bf8958
Dmitrii Avramenko <dmitriiavramenko@Dmitriis-MacBook-Air.local>
2023-09-30 14:00:28 +0300

    init

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/banditmoscow1337/telegraph
+
+go 1.21.0
+
+require (
+	github.com/banditmoscow1337/utils v0.1.11
+	github.com/goccy/go-json v0.10.2
+)
+
+require (
+	github.com/rs/dnscache v0.0.0-20230804202142-fc85eb664529 // indirect
+	golang.org/x/sync v0.3.0 // indirect
+)
---
+++ utils.go
@@ -0,0 +1,69 @@
+package telegraph
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/banditmoscow1337/utils/net"
+	"github.com/goccy/go-json"
+)
+
+func (t *Telegraph) call(uri string, querys []Query, body []byte) (ret []byte, err error) {
+	var (
+		header = http.Header{}
+		resp   *http.Response
+		data   []byte
+		r      Result
+	)
+
+	method := "GET"
+
+	if len(body) > 0 {
+		header.Add("Content-Type", "application/json")
+		method = "POST"
+	} else {
+		uri += "?access_token=" + t.a.AccessToken + "&"
+		for _, q := range querys {
+			uri += q.Name + "=" + url.QueryEscape(q.Value) + "&"
+		}
+	}
+
+	resp, err = t.n.Do("https://api.telegra.ph/"+uri, method, 2*time.Second, header, bytes.NewBuffer(body))
+	if err != nil {
+		return
+	}
+
+	if resp.StatusCode != 200 {
+		err = errors.New(resp.Status)
+		return
+	}
+
+	if data, err = io.ReadAll(resp.Body); err != nil {
+		return
+	}
+
+	err = json.Unmarshal(data, &r)
+	if err != nil {
+		return
+	}
+
+	if !r.Ok {
+		err = errors.New(r.Error)
+	}
+
+	ret = r.Result
+
+	return
+}
+
+func Init(token ...string) (t Telegraph, err error) {
+	if len(token) > 0 {
+		t.a.AccessToken = token[0]
+	}
+	t.n = net.Init()
+	return
+}
